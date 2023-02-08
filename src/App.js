7144c11f57dc094f996e368df961d0798f9e8c2d
Muhammad Ali <aikalmine@gmail.com>
2023-02-08 23:03:44 +0500

    some fixing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Form from './Form';
+import FormInput from './FormInput';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    emailAddress: '',
+    password: '',
+  };
+
+  const submit = (form) => {
+    setMessage(`Thanks for signing up, ${form.firstName} ${form.lastName}! We've sent you an email to ${form.emailAddress}.`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sign Up</h1>
+
+      <Form submit={submit} initialValues={initialValues}>
+        <FormInput 
+          label="First Name" 
+          name="firstName" />
+        <FormInput 
+          label="Last Name" 
+          name="lastName" />
+        <FormInput 
+          label="Email Address" 
+          type="email" 
+          name="emailAddress" />
+        <FormInput 
+          label="Password" 
+          type="password" 
+          name="password" />
+      </Form>
+
+      <p>{message}</p>
+
+      <h2>Log In</h2>
+      <Form
+        submit={(form) => {
+          alert(`Logged in as ${form.username}!`);
+        }}
+        initialValues={{
+          username: '',
+          password: ''
+        }}>
+        <FormInput
+          label="Username"
+          name="username" />
+        <FormInput
+          label="Password"
+          name="password" />
+      </Form>
     </div>
   );
 }
